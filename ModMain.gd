56a121858448ce727697832fd84f582f4e06c0f3
JCalebBR <josshuacaleb@gmail.com>
2026-06-17 14:12:19 -0300

    Fix symver in manifest and main gd

--- ModMain.gd
+++ ModMain.gd
@@ -7,7 +7,7 @@ const MOD_PRIORITY = -99
 const MOD_NAME = "Ganymede Runners' Aftermarket Huds"
 const MOD_VERSION_MAJOR = 0
 const MOD_VERSION_MINOR = 2
-const MOD_VERSION_BUGFIX = 0
+const MOD_VERSION_BUGFIX = 1
 const MOD_VERSION_METADATA = ""
 # Path of the mod folder, automatically generated on runtime
 var modPath:String = get_script().resource_path.get_base_dir() + "/"
